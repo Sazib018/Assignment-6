e0e0fce326e5571aa5f279c5f31a53b94837ac41
Sazib <mahiburislamsazib@gmail.com>
2024-10-01 19:36:19 +0600

    dsfsd

--- js/index.js
+++ js/index.js
@@ -113,59 +113,28 @@ function readPost(name, view) {
     showSec.appendChild(showSms);
 }
 
-const latestPost = document.getElementById('latest-post')
-async function lastPost() {
-    const res = await fetch(
-        `https://openapi.programming-hero.com/api/retro-forum/latest-posts`
-    );
-    const data = await res.json();
-    const posts = data.posts;
-    anotherPost(posts);
 
-}
-lastPost();
-function anotherPost(posts) {
-    for (const post of posts) {
-        const cards = document.createElement('div')
-        cards.classList =`bg-gray-100 flex items-center justify-center h-screen`
-        cards.innerHTML = `
-           <div class="">
-                <div class="bg-white rounded-lg shadow-lg p-6 w-80">
-                    <img src="${post.cover_image}" alt="" class="bg-gray-200 h-36 rounded-md mb-4">
-                    <div class="text-gray-500 text-sm mb-2 flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <g clip-path="url(#clip0_29_1881)">
-                              <path d="M4 7C4 6.46957 4.21071 5.96086 4.58579 5.58579C4.96086 5.21071 5.46957 5 6 5H18C18.5304 5 19.0391 5.21071 19.4142 5.58579C19.7893 5.96086 20 6.46957 20 7V19C20 19.5304 19.7893 20.0391 19.4142 20.4142C19.0391 20.7893 18.5304 21 18 21H6C5.46957 21 4.96086 20.7893 4.58579 20.4142C4.21071 20.0391 4 19.5304 4 19V7Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M16 3V7" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M8 3V7" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M4 11H20" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M11 16C11 16.2652 11.1054 16.5196 11.2929 16.7071C11.4804 16.8946 11.7348 17 12 17C12.2652 17 12.5196 16.8946 12.7071 16.7071C12.8946 16.5196 13 16.2652 13 16C13 15.7348 12.8946 15.4804 12.7071 15.2929C12.5196 15.1054 12.2652 15 12 15C11.7348 15 11.4804 15.1054 11.2929 15.2929C11.1054 15.4804 11 15.7348 11 16Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            </g>
-                            <defs>
-                              <clipPath id="clip0_29_1881">
-                                <rect width="24" height="24" fill="white"/>
-                              </clipPath>
-                            </defs>
-                          </svg>
-                       <p>${post.posted_date}</p>
-                    </div>
-                    <h3 class="text-lg font-extrabold mb-3">
-                       ${post.title}
-                    </h3>
-                    <p class="text-sm text-gray-600 mb-4">
-                       ${post.description}
-                    </p>
-                    <div class="flex items-center">
-                        <img src="${post.profile_image}" alt="Author Image" class="rounded-full h-10 w-10 mr-3">
-                        <div>
-                            <p class="text-gray-800 font-semibold">${post.author.name}</p>
-                            <p class="text-gray-500 text-sm">Unknown</p>
-                        </div>
-                    </div>
-                </div>
-            </div> `
-        anotherPost.appendChild(cards)
-    }
-}
 
+
+const detailsButton = async () => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+  const data = await res.json();
+
+  const cardsBody = document.getElementById('latest-container');
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+      <div>
+                <img class ="h-9" src="${data.cover_image}" alt="">
+            </div>
+            <div>
+                <h3>${data.title}</h3>
+                <p>${data.description}</p>
+            </div>
+            <div>
+                <img src="${data.profile_image}" alt="">
+            </div>
+  `;
+  cardsBody.appendChild(div);
+};
 
